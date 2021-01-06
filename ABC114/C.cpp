64549dc5dfb54bcff001af8dc3a7bde584358660
allagenovese <hnasu95@gmail.com>
2021-01-06 20:53:56 +0900

    add ABC114/C.cpp

--- ABC114/C.cpp
+++ ABC114/C.cpp
@@ -4,21 +4,21 @@ const double PI = acos(-1);
 using namespace std;
 
 void dfs(long long x, long long  n,  bool f3, bool f5, bool f7,  
-		long long & count){
-	    if(x > n) return;
+long long & count){
+    if(x > n) return;
 
-	        if(f3) if(f5) if(f7) count++;
+    if(f3) if(f5) if(f7) count++;
 
-		    dfs( 10*x+ 3, n, true, f5, f7, count );
-		        dfs( 10*x+ 5, n, f3, true, f7, count );
-			    dfs( 10*x+ 7, n, f3, f5, true, count );
+    dfs( 10*x+ 3, n, true, f5, f7, count );
+    dfs( 10*x+ 5, n, f3, true, f7, count );
+    dfs( 10*x+ 7, n, f3, f5, true, count );
 }
 
 int main(){
-	    long long n;
-	        cin >> n;
-		    long long count = 0;
-		        dfs(0, n, false, false, false, count);
-			    cout << count << endl;
+    long long n;
+    cin >> n;
+    long long count = 0;
+    dfs(0, n, false, false, false, count);
+    cout << count << endl;
 
 }
